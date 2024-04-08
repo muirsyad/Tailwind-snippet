80268b2ce1d93a72f8d77ec0b60f9a8098d8d536
Muhammad Irsyad <muirsyad@synergy-group.com.my>
2024-04-08 16:45:07 +0800

    add new route for sidebar

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,12 @@ import { Routes } from '@angular/router';
 import { SelectionpageComponent } from './Pages/selectionpage/selectionpage.component';
 import { NavPageComponent } from './Pages/nav-page/nav-page.component';
 import { PlaygroundPageComponent } from './Pages/playground-page/playground-page.component';
+import { SidePageComponent } from './Pages/side-page/side-page.component';
 
 export const routes: Routes = [
     { path: '', component: SelectionpageComponent },
     { path: 'navbar', component: NavPageComponent },
     { path: 'play', component: PlaygroundPageComponent },
+    { path: 'sidebar', component: SidePageComponent },
+
 ];
